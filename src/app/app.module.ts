2e5b83dc2448ff3309064e30969c00094b993545
gbsantstech <contatogbsants@outlook.com>
2022-11-24 14:51:29 -0300

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ROUTERS} from './app.routes';
+import { RouterModule } from '@angular/router';
+import { FuncionarioModule } from './funcionario';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTERS),
+    FuncionarioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/funcionario.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Funcionario } from './funcionario/funcionario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncionarioService {
+
+  constructor() { }
+
+  listarTodos(): Funcionario[]{
+    const funcionarios = localStorage['funcionarios'];
+    return funcionarios ? JSON.parse(funcionarios) : [];
+  }
+
+  cadastrar(funcionario: Funcionario): void{
+    const funcionarios = this.listarTodos();
+    funcionario.id = new Date().getTime();
+    funcionarios.push(funcionario);
+    localStorage['funcionarios'] = JSON.stringify(funcionarios);
+  }
+
+  buscarPorId(id:number): Funcionario{
+    const funcionarios: Funcionario[] = this.listarTodos();
+    return funcionarios.find(funcionario => funcionario.id === id);
+  }
+
+  atualizar(funcionario: Funcionario):void{
+    const funcionarios: Funcionario[] = this.listarTodos();
+    funcionarios.forEach((funcionario, pos, funcionarios) =>{
+      if(funcionario.id === funcionario.id){
+        funcionarios[pos] = funcionario;
+      }
+    });
+    localStorage['funcionarios'] = JSON.stringify(funcionarios);
+  }
+}
